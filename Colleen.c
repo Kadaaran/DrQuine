34d5e4571a5c7fbec0a8bfc9b3870dd18788d6e9
Kassandre <kpedro@student.42.fr>
2016-06-23 00:53:53 +0200

    finitoooo

--- Colleen.c
+++ Colleen.c
@@ -2,4 +2,5 @@
 /*
 coucou
 */
-int main(){char*a="#include<stdio.h>%c/*%ccoucou%c*/%cint%cmain(){char*a=%c%s%c;printf(a,10,10,10,10,32,34,a,34,10);}%c";printf(a,10,10,10,10,32,34,a,34,10);}
+int test();
+int main(){/*test*/char*a="#include<stdio.h>%c/*%ccoucou%c*/%cint%ctest();%cint%cmain(){/*test*/char*a=%c%s%c;printf(a,10,10,10,10,32,10,32,34,a,34,10);}%c";printf(a,10,10,10,10,32,10,32,34,a,34,10);}
