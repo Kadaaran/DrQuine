ffb2f384d73ca680b13eb4bf556cbd2c4b1ecac0
Kass <kpedro@student.42.fr>
2016-06-22 20:02:31 +0200

    almost finish first yay

--- Colleen.c
+++ Colleen.c
@@ -2,5 +2,4 @@
 /*
 coucou
 */
-uesh();
-int main(){/*uesh*/char*a="#include<stdio.h>%c/*%ccoucou%c*/%cuesh();%cint%cmain(){char*a=%c%s%c;printf(a,10,10,10,10,10,32,34,a,34,10);}%c";printf(a,10,10,10,10,10,32,34,a,34,10);}
+int main(){char*a="#include<stdio.h>%c/*%ccoucou%c*/%cint%cmain(){char*a=%c%s%c;printf(a,10,10,10,10,32,34,a,34,10);}%c";printf(a,10,10,10,10,32,34,a,34,10);}
