6e3b465ecb39356f618ef10d62f34edc0b16db28
Kass <kpedro@student.42.fr>
2016-06-22 19:43:01 +0200

    good file

---
+++ Colleen.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+/*
+coucou
+*/
+uesh();
+int main(){/*uesh*/char*a="#include<stdio.h>%c/*%ccoucou%c*/%cuesh();%cint%cmain(){char*a=%c%s%c;printf(a,10,10,10,10,10,32,34,a,34,10);}%c";printf(a,10,10,10,10,10,32,34,a,34,10);}
