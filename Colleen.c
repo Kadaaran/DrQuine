d332d71caabe5c5e4a4198452e02b9acb2ec24ca
Kass <kpedro@student.42.fr>
2016-06-22 19:33:56 +0200

    first exercice in progress

---
+++ Colleen.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+/*
+coucou
+*/
+int main(){char*a="#include<stdio.h>%c/*%ccoucou%c*/%cint%cmain(){char*a=%c%s%c;printf(a,10,10,10,10,32,34,a,34,10);}%c";printf(a,10,10,10,10,32,34,a,34,10);}
