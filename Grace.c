f87244c351ac557d61e77bcd5e76bec2b8c4febd
Kass <kpedro@student.42.fr>
2016-06-23 18:42:31 +0200

    clean version

--- Grace.c
+++ Grace.c
@@ -4,5 +4,5 @@
 */
 #define UNO
 #define DOS
-#define FT(x) int main(){FILE*f = fopen("Grace_kid.c", "w");char*a="#include<stdio.h>%c/*%c%cuesh%c*/%c#define%cUNO%c#define%cDOS%c#define%cFT(x)%cint%cmain(){FILE*f = fopen(%cGrace_kid.c%c, %cw%c);char*a=%c%s%c;fprintf(f,a,10,10,9,10,10,32,10,32,10,32,32,32,34,34,34,34,34,a,34,10,10);}%cFT(42)%c";fprintf(f,a,10,10,9,10,10,32,10,32,10,32,32,32,34,34,34,34,34,a,34,10,10);}
+#define FT(x) int main(){FILE*f = fopen("Grace_kid.c", "w");char*a="#include<stdio.h>%1$c/*%1$c%2$cuesh%1$c*/%1$c#define UNO%1$c#define DOS%1$c#define FT(x) int main(){FILE*f = fopen(%3$cGrace_kid.c%3$c, %3$cw%3$c);char*a=%3$c%4$s%3$c;fprintf(f,a,10,9,34,a);}%1$cFT(42)%1$c";fprintf(f,a,10,9,34,a);}
 FT(42)
