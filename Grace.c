4e28c9e12e570d57f4085f376f64dbbaf48cbb47
Kass <kpedro@student.42.fr>
2016-06-23 15:05:12 +0200

    second finitooooo

---
+++ Grace.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/*
+uesh
+*/
+#define UNO
+#define DOS
+#define FT(x) int main(){char*a="#include<stdio.h>%c/*%cuesh%c*/%c#define%cUNO%c#define%cDOS%c#define%cFT(x)%cint%cmain(){char*a=%c%s%c;printf(a,10,10,10,10,32,10,32,10,32,32,32,34,a,34,10,10);}%cFT(42)%c";printf(a,10,10,10,10,32,10,32,10,32,32,32,34,a,34,10,10);}
+FT(42)
