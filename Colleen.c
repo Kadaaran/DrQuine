40bd1f66d643ee8c49e52bca530c2b6ec491cecf
Kass <kpedro@student.42.fr>
2016-06-23 15:32:48 +0200

    add tab

--- Colleen.c
+++ Colleen.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /*
-coucou
+	coucou
 */
 int test();
-int main(){/*test*/char*a="#include<stdio.h>%c/*%ccoucou%c*/%cint%ctest();%cint%cmain(){/*test*/char*a=%c%s%c;printf(a,10,10,10,10,32,10,32,34,a,34,10);}%c";printf(a,10,10,10,10,32,10,32,34,a,34,10);}
+int main(){/*test*/char*a="#include<stdio.h>%c/*%c%ccoucou%c*/%cint%ctest();%cint%cmain(){/*test*/char*a=%c%s%c;printf(a,10,10,9,10,10,32,10,32,34,a,34,10);}%c";printf(a,10,10,9,10,10,32,10,32,34,a,34,10);}
--- Grace.c
+++ Grace.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /*
-uesh
+	uesh
 */
 #define UNO
 #define DOS
-#define FT(x) int main(){FILE*f = fopen("Grace_kid.c", "w");char*a="#include<stdio.h>%c/*%cuesh%c*/%c#define%cUNO%c#define%cDOS%c#define%cFT(x)%cint%cmain(){FILE*f = fopen(%cGrace_kid.c%c, %cw%c);char*a=%c%s%c;fprintf(f,a,10,10,10,10,32,10,32,10,32,32,32,34,34,34,34,34,a,34,10,10);}%cFT(42)%c";fprintf(f,a,10,10,10,10,32,10,32,10,32,32,32,34,34,34,34,34,a,34,10,10);}
+#define FT(x) int main(){FILE*f = fopen("Grace_kid.c", "w");char*a="#include<stdio.h>%c/*%c%cuesh%c*/%c#define%cUNO%c#define%cDOS%c#define%cFT(x)%cint%cmain(){FILE*f = fopen(%cGrace_kid.c%c, %cw%c);char*a=%c%s%c;fprintf(f,a,10,10,9,10,10,32,10,32,10,32,32,32,34,34,34,34,34,a,34,10,10);}%cFT(42)%c";fprintf(f,a,10,10,9,10,10,32,10,32,10,32,32,32,34,34,34,34,34,a,34,10,10);}
 FT(42)
