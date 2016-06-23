ea628c3ad4a92a37d0327d4ef77436c21691d56a
Kass <kpedro@student.42.fr>
2016-06-23 15:16:04 +0200

    Real finitoooo

--- Grace.c
+++ Grace.c
@@ -4,5 +4,5 @@ uesh
 */
 #define UNO
 #define DOS
-#define FT(x) int main(){char*a="#include<stdio.h>%c/*%cuesh%c*/%c#define%cUNO%c#define%cDOS%c#define%cFT(x)%cint%cmain(){char*a=%c%s%c;printf(a,10,10,10,10,32,10,32,10,32,32,32,34,a,34,10,10);}%cFT(42)%c";printf(a,10,10,10,10,32,10,32,10,32,32,32,34,a,34,10,10);}
+#define FT(x) int main(){FILE*f = fopen("Grace_kid.c", "w");char*a="#include<stdio.h>%c/*%cuesh%c*/%c#define%cUNO%c#define%cDOS%c#define%cFT(x)%cint%cmain(){FILE*f = fopen(%cGrace_kid.c%c, %cw%c);char*a=%c%s%c;fprintf(f,a,10,10,10,10,32,10,32,10,32,32,32,34,34,34,34,34,a,34,10,10);}%cFT(42)%c";fprintf(f,a,10,10,10,10,32,10,32,10,32,32,32,34,34,34,34,34,a,34,10,10);}
 FT(42)
